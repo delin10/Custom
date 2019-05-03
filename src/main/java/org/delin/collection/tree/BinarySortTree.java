dd3735ac558ca9333fdca94671cfcd5929388211
delin10 <23187420+delin10@users.noreply.github.com>
2019-05-03 16:29:28 +0800

    修改二叉查找树replace方法bug和增强insert方法功能
    -replace存在根结点双亲结点不设置null的情况
    -insert方法增加suppier、consumer参数

--- src/main/java/org/delin/collection/tree/BinarySortTree.java
+++ src/main/java/org/delin/collection/tree/BinarySortTree.java
@@ -2,6 +2,8 @@ package org.delin.collection.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 /**
  * 二叉搜索树
@@ -24,12 +26,12 @@ public class BinarySortTree<K extends Comparable, V> implements ITree<K, V> {
 
     @Override
     public Node<K, V> insertRepeatablily(K key, V value) {
-        return insert(key, value, root, true);
+        return insert(key, value, getRoot(), true, null, null, null, () -> new Node());
     }
 
     @Override
     public Node<K, V> insertOrUpdate(K key, V value) {
-        return insert(key, value, root, false);
+        return insert(key, value, getRoot(), false, null, null, null, () -> new Node());
     }
 
     @Override
@@ -126,29 +128,49 @@ public class BinarySortTree<K extends Comparable, V> implements ITree<K, V> {
         return searchProcessor(node.getRight());
     }
 
-    private Node<K, V> insert(K key, V value, Node<K, V> parent, boolean repeatable) {
-        if (root == null) {
-            return root = new Node<>(key, value);
+    protected Node<K, V> insert(K key, V value, Node<K, V> parent, boolean repeatable, Consumer<Node<K, V>> progress, Consumer<Node<K, V>> complete, Consumer<Node<K, V>> after, Supplier<Node<K, V>> supplier) {
+        if (getRoot() == null) {
+            Node<K, V> newRoot = newNode(supplier, key, value);
+            setRoot(newRoot);
+            if (complete != null) {
+                complete.accept(newRoot);
+            }
+            return newRoot;
+        }
+        if (progress != null) {
+            progress.accept(parent);
         }
         Node<K, V> node = null;
         //如果小于关键字 或者 可以插入相同关键字
         if (key.compareTo(parent.getKey()) < 0 || (repeatable && key.compareTo(parent.getKey()) == 0)) {
             if (parent.getLeft() != null) {
-                node = insert(key, value, parent.getLeft(), repeatable);
+                node = insert(key, value, parent.getLeft(), repeatable, progress, complete, after, supplier);
             } else {
-                node = new Node<>(key, value);
+                node = newNode(supplier, key, value);
                 setLeftChild(parent, node);
+                if (complete != null) {
+                    complete.accept(node);
+                }
             }
         } else if (key.compareTo(parent.getKey()) > 0) {//大于关键字
             if (parent.getRight() != null) {
-                node = insert(key, value, parent.getRight(), repeatable);
+                node = insert(key, value, parent.getRight(), repeatable, progress, complete, after, supplier);
             } else {
-                node = new Node<>(key, value);
+                node = newNode(supplier, key, value);
                 setRightChild(parent, node);
+                if (complete != null) {
+                    complete.accept(node);
+                }
             }
         } else {//等于关键字
             parent.setData(value);
             node = parent;
+            if (complete != null) {
+                complete.accept(node);
+            }
+        }
+        if (after != null) {
+            after.accept(parent);
         }
         return node;
     }
@@ -160,16 +182,17 @@ public class BinarySortTree<K extends Comparable, V> implements ITree<K, V> {
      * @param newNode
      * @param inherit 是否继承cur的子节点
      */
-    private void replace(Node<K, V> cur, Node<K, V> newNode, boolean inherit) {
+    protected void replace(Node<K, V> cur, Node<K, V> newNode, boolean inherit) {
         Node<K, V> parent = cur.getParent();//if root, parent is null
-        if (cur != root) {
+        if (cur != getRoot()) {
             if (parent.getRight() == cur) {
                 setRightChild(parent, newNode);
             } else {
                 setLeftChild(parent, newNode);
             }
         } else {
-            root = newNode;
+            setRoot(newNode);
+            newNode.setParent(null);
         }
         if (inherit) {
             if (newNode != null) {
@@ -180,17 +203,28 @@ public class BinarySortTree<K extends Comparable, V> implements ITree<K, V> {
     }
 
 
-    private void setLeftChild(Node<K, V> node, Node<K, V> child) {
+    protected void setLeftChild(Node<K, V> node, Node<K, V> child) {
         node.setLeft(child);
         if (child != null) {
             child.setParent(node);
         }
     }
 
-    private void setRightChild(Node<K, V> node, Node<K, V> child) {
+    protected void setRightChild(Node<K, V> node, Node<K, V> child) {
         node.setRight(child);
         if (child != null) {
             child.setParent(node);
         }
+    }
+
+    protected void setRoot(Node<K, V> root) {
+        this.root = root;
+    }
+
+    protected Node<K, V> newNode(Supplier<Node<K, V>> supplier, K key, V value) {
+        Node<K, V> node = supplier.get();
+        node.setKey(key);
+        node.setData(value);
+        return node;
     }
 }
