23738e4bbff23021d384914c39e4f4dca78cfb64
delin10 <23187420+delin10@users.noreply.github.com>
2019-05-04 15:03:11 +0800

    增加测试deleteFirst

--- src/test/java/org/delin/collection/tree/BalancedBinaryTreeTest.java
+++ src/test/java/org/delin/collection/tree/BalancedBinaryTreeTest.java
@@ -10,17 +10,32 @@ public class BalancedBinaryTreeTest {
             System.out.println("insert ========>" + key);
             tree.insertOrUpdate(key, key);
         }
+        System.out.println(tree.getSize());
         TreeUtils.midorder(tree.getRoot(), n -> {
             System.out.print(n + " ");
         });
         Random random = new Random();
-        for (int i = 0; i < 10; ++i) {
-            int key = random.nextInt(100);
-            System.out.println("-------------->insert " + key);
+//        for (int i = 0; i < 10; ++i) {
+//            int key = random.nextInt(100);
+//            System.out.println("-------------->insert " + key);
+//
+//            Node node = tree.insertOrUpdate(key, random.nextInt(100000));
+//            System.out.println("insert " + node + "==>" + node.getParent());
+//        }
+//        TreeUtils.midorder(tree.getRoot(), n -> {
+//            System.out.print(n + " ");
+//        });
+//        System.out.println();
 
-            Node node = tree.insertOrUpdate(key, random.nextInt(100000));
-            System.out.println("insert " + node + "==>" + node.getParent());
+        for (int i = 0; i < 100; ++i) {
+            int key=random.nextInt(100);
+            System.out.println("-------------->delete " + key);
+            Node node = tree.deleteFirst(key);
+            if (node != null) {
+                System.out.println("delete " + node);
+            }
         }
+        System.out.println(tree.getSize());
         TreeUtils.midorder(tree.getRoot(), n -> {
             System.out.print(n + " ");
         });
